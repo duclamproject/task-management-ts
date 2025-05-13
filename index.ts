69c9aba13e67eefcab927ff09f1048d56e88e2b2
Duc <ngohoaiduc08@gmail.com>
2025-05-13 16:55:24 +0700

    Done: Get all task

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Oke 123");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+import Task from "./modules/task.model";
+
+dotenv.config();
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false,
+  });
+  console.log(tasks);
+
+  res.json({ tasks });
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
