5d20c5e16c85b2d4e43a0239ea51ee4eb3c8bb37
Duc <ngohoaiduc08@gmail.com>
2025-05-13 22:20:27 +0700

    Done: Sap xep theo tieu chi

--- api/controllers/task.controller.ts
+++ api/controllers/task.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Task from "../modules/task.model";
 
 export const index = async (req: Request, res: Response) => {
+  // Filter
   interface Find {
     deleted: boolean;
     status?: string;
@@ -12,8 +13,18 @@ export const index = async (req: Request, res: Response) => {
   if (req.query.status) {
     find.status = req.query.status.toString();
   }
-  const tasks = await Task.find(find);
-  console.log(tasks);
+  // End Filter
+
+  // Sort
+  let sort = {};
+  if (req.query.sortKey && req.query.sortValue) {
+    const sortKey = req.query.sortKey.toLocaleString();
+    sort[sortKey] = req.query.sortValue;
+  }
+  // End Sort
+
+  const tasks = await Task.find(find).sort(sort);
+  //   console.log(tasks);
 
   res.json({ tasks });
 };
