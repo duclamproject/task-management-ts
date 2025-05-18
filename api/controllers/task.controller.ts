0ad8d641e9b716b7aa74d0d7aa04c0c298b69026
Duc <ngohoaiduc08@gmail.com>
2025-05-18 14:48:41 +0700

    Done: Change-status

--- api/controllers/task.controller.ts
+++ api/controllers/task.controller.ts
@@ -67,3 +67,26 @@ export const detail = async (req: Request, res: Response) => {
 
   res.json({ task });
 };
+
+export const changeStatus = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+    const status: string = req.body.status;
+    await Task.updateOne(
+      { _id: id },
+      {
+        status: status,
+      }
+    );
+
+    res.json({
+      code: 200,
+      message: "Cập nhật trạng thái thành công!",
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Không tồn tại!",
+    });
+  }
+};
