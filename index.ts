7c48c48b7a5d895be1b3196277c5a2cb7bfb39df
Duc <ngohoaiduc08@gmail.com>
2025-05-18 21:17:09 +0700

    Done: CORS

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import * as database from "./config/database";
 import mainV1Routes from "./api/routes/index.route";
 
@@ -11,6 +12,8 @@ const port: number | string = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 mainV1Routes(app);
 
